232344dc1da9bb9ab5f2ae41a575194dd482d74c
Fadlan Fadilah <fhadlan123@gmail.com>
2025-01-02 14:26:55 +0700

    disqus comment

--- app/Views/frontend/pages/single_post.php
+++ app/Views/frontend/pages/single_post.php
@@ -81,28 +81,26 @@
         </div>
         <div class="mt-5">
             <div id="disqus_thread"></div>
-            <script type="application/javascript">
-                var disqus_config = function() {
-
-
-
+            <script>
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
                 };
-                (function() {
-                    if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
-                        document.getElementById('disqus_thread').innerHTML = 'Disqus comments not available by default when the website is previewed locally.';
-                        return;
-                    }
+                */
+
+                document.addEventListener('DOMContentLoaded', function() {
                     var d = document,
                         s = d.createElement('script');
-                    s.async = true;
-                    s.src = '//' + "themefisher-template" + '.disqus.com/embed.js';
+                    s.src = 'https://fadlan-1.disqus.com/embed.js';
                     s.setAttribute('data-timestamp', +new Date());
                     (d.head || d.body).appendChild(s);
-                })();
+                });
             </script>
-            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
-            </noscript>
-            <a href="https://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
         </div>
     </div>
     <div class="col-lg-4">
